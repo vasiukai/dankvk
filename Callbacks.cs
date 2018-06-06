be9b346719c45a1b42c0fbebb0a7ac9ae86af414
danuixas <bernotasdanas@gmail.com>
2018-06-06 11:22:19 +0300

    Delete Callbacks.cs

--- Callbacks.cs
+++
@@ -1,11 +0,0 @@
-﻿[BoltGlobalBehaviour(BoltNetworkModes.Host, "Level1")]
-public class Callbacks : Bolt.GlobalEventListener
-{
-    public override void LoadSceneLocal(string map)
-    {
-        BoltNetwork.Instantiate(BoltPrefabs.Player);
-    }
-    public override void LoadSceneRemote(BoltConnection connection)
-    {
-        BoltNetwork.Instantiate(BoltPrefabs.Player);
-    }}
